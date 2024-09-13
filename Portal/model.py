16444c6bd9edc6599dc7e78c576643bfca828f64
Gentle-Community <justinemmanuel331@gmail.com>
2024-09-13 18:53:04 +0100

    commit

--- Portal/model.py
+++ Portal/model.py
@@ -4,7 +4,7 @@ from Portal import db
 class SoilDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.String(80), nullable=False)
-    moisture = db.Column(db.String(120), unique=True, nullable=False)
+    moisture = db.Column(db.String(120), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
